f72a3800d88c681248f474450ca54f0852c8c592
unknown <stud_17@17_04.efdnew.isuct.ru>
2015-10-10 12:58:25 +0400

    change file

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,5 +1,5 @@
 public class MyFirstApp {
     public static void main (String[] args){
-        System.out.print ("Hello world");
+        System.out.print("Vlasova Elvira Andreevna");
     }
-)
+}
