e5813dd815471c193283bb8eac8ee17812740d15
unknown <stud_17@17_08.efdnew.isuct.ru>
2015-10-10 13:06:30 +0400

    Added first app

--- FirstApp.java
+++ FirstApp.java
@@ -1,7 +1,7 @@
 public class FirstApp {
     public static void main(String[] args){
-	System.out.println("Hello world");
-	System.out.println("You said: " + args[0]);
+	System.out.println("Kuznetsova Irina Alekseevna");
+	
 	
     }
 }
