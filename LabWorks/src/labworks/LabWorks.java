7b6e48e3f5564f7f456a53b595b9d2d91fe3f031
unknown <stud_17@17_05.efdnew.isuct.ru>
2015-10-17 13:33:16 +0400

    prostie typi

---
+++ LabWorks/src/labworks/LabWorks.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author stud_17
+ */
+public class LabWorks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean logic = true;
+        System.out.print(logic);
+        char charVar = 'a';
+        System.out.print(charVar);
+        byte someByte = 12;
+        System.out.print(someByte);
+        short j = 19;
+        System.out.print(j);
+        int z = 27;
+        System.out.print(z);
+        long r = 30;
+        System.out.print(r);
+        float f = 32f;
+        System.out.print(f);
+        double k = 55;
+        System.out.print(k);
+        
+        
+        
+    }
+}
