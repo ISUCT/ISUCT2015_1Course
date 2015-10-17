685ecb6ef59eff886b51c2414d0d904e7b31f478
unknown <stud_17@17_15.efdnew.isuct.ru>
2015-10-17 13:30:08 +0400

    Lab

---
+++ LabWorks/src/labworks/LabWorks.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author stud_17
+ */
+public class LabWorks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean logic = true; //false
+        System.out.println(logic);
+        char charVar = 'a';
+        System.out.println(charVar);
+        byte someByte = 12;
+        System.out.println(someByte);
+        short someShort = 13;
+        System.out.println(someShort);
+        int someInt = 14;
+        System.out.println(someInt);
+        long someLong = 15;
+        System.out.println(someLong);
+        float someFloat = 16;
+        System.out.println(someFloat);
+        double someDouble = 17;
+        System.out.println(someDouble);
+    }
+}
