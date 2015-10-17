c0e45e3ee8cbaead2467ce7a99f71fdd08fd61b1
unknown <stud_17@17_01.efdnew.isuct.ru>
2015-10-17 13:28:30 +0400

    Add java project

---
+++ LabWorks/src/labworks/LabWorks.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author stud_17
+ */
+public class LabWorks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean logic = false; 
+       System.out.println(logic);
+       char charVar = 'a';
+       System.out.println(charVar);
+       byte b = 66; 
+       System.out.println(b);
+       short s; 
+       s = b; 
+       System.out.println(s);
+       int x = (int) 12; 
+       System.out.println(x);
+       long dog = 6696669; 
+       System.out.println (dog); 
+       float h = 99.8f; 
+       System.out.println(h);
+       double p = 5678.82; 
+       System.out.println(p);
+       
+       
+       
+    }
+}
