2dbf960beb74e53fcf835d04aee171c80aefb09a
Your Name <you@example.com>
2015-10-17 13:32:40 +0400

    simpletypes

---
+++ LabWorks/src/labworks/LabWorks.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author stud_17
+ */
+public class LabWorks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean logic = true;
+        System.out.println(logic);
+        char charVar = 'a';
+        System.out.println(charVar);
+        byte someByte = 12 ;
+        System.out.println (someByte);
+        short someShort = 5;
+        System.out.println (someShort);
+        int someInt = 8;
+        System.out.println (someInt);
+        long someLong = 6;
+        System.out.println (someLong);
+        float someFloat = 4;
+        System.out.println (someFloat);
+        double someDouble = 7;
+        System.out.print (someDouble);
+                
+    } 
+}
+   
