73feae1e9a83246b297acb3347939ca29131771d
kosh0655 <kosh0645@yandex.ru>
2015-10-17 14:44:56 +0300

    Labs

---
+++ LabWorks/src/labworks/Lab1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author Алена
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean log = true; //or false;
+        char charP = 'k';
+        byte j = 22;
+        short q = 128;
+        int i = 15987;
+        long f = 56876211;
+        float m = 241.87f;
+        double r = 5688.10;
+        
+        System.out.println (log);
+        System.out.println (charP);
+        System.out.println (j);
+        System.out.println (q);
+        System.out.println (i);
+        System.out.println (f);
+        System.out.println (m);
+        System.out.println (r);
+        
+    }
+    
+}
