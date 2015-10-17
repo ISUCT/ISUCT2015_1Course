33687863c59823d535bf9cb92577c81afee7c512
unknown <jskonst@17_00.efdnew.isuct.ru>
2015-10-17 13:05:58 +0400

    Added simple Lab work

---
+++ LabWorks/src/labworks/LabWorks.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author jskonst
+ */
+public class LabWorks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean logic = true; //false
+        System.out.println(logic);
+        char charVar = 'a';
+        System.out.println(charVar);
+        //.....
+    }
+}
