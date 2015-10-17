da7a1bc09f204824580e4d666e4ddabc0b8f1f90
unknown <stud_17@17_04.efdnew.isuct.ru>
2015-10-17 13:32:57 +0400

    simpletypes

---
+++ LabWorks/src/labworks/LabWorks.java
@@ -0,0 +1,34 @@
+ /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author stud_17
+ */
+public class LabWorks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean logic = true;
+        System.out.println (logic);
+        char charVar = 'a';
+        System.out.println(charVar);
+        byte someByte = 12;
+        System.out.println (someByte);
+        short someShort = 7;
+        System.out.println (someShort);
+        int someInt = 17;
+        System.out.println (someInt);
+        long someLong = 10;
+        System.out.println (someLong);
+        float someFloat = 11;
+        System.out.println (someFloat);
+        double someDouble = 5;
+        System.out.println (someDouble);
+    }
+}
