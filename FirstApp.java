7355ddcb4ee52ce263f53c24d3a851542007ae43
unknown <stud_17@17_08.efdnew.isuct.ru>
2015-10-17 13:54:51 +0400

    lr

--- FirstApp.java
+++ FirstApp.java
@@ -1,14 +1,7 @@
 public class FirstApp {
     public static void main(String[] args){
-<<<<<<< HEAD
-	System.out.println("Kuznetsova Irina Alekseevna");
-	
-	
-    }
-}
-=======
 	System.out.println("Klenina_N_A");
 	
     }
 }
->>>>>>> d56aba64433d4eda64aca95852748f9faee7423f
+
