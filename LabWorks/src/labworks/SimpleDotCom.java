3fb87df2b08ee669f1d05238ab77e5e318b4b9d8
Anna <annazahk15@gmail.com>
2015-11-23 13:48:46 +0300

    homework

---
+++ LabWorks/src/labworks/SimpleDotCom.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author Анютка
+ */
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+    public void setLocationCells(int[] locs) {
+    locationCells = locs;
+     }
+    public String checkYourself(String stringGuess) {
+      int guess = Integer.parseInt(stringGuess);
+      String result = "Мимо";
+      for (int cell : locationCells) {
+        if (guess == cell) {
+         result = "Попал";
+         numOfHits++;
+         break;
+        }  
+      }
+      if (numOfHits ==
+              locationCells.length) {
+              result = "Потопил";
+      }
+      System.out.println(result);
+      return result;
+      }
+}
