08bc9fb39f86fe1688a02848d595712241e77811
Mary <lllavrucha1308@yandex.ru>
2015-12-04 18:25:07 +0300

    classes

--- LabWorks/src/GameHelper/DotCom.java
+++ LabWorks/src/GameHelper/DotCom.java
@@ -29,6 +29,16 @@ public class DotCom {
         int index = locationCells.indexOf(userInput);
         if (index >= 0) {
             locationCells.remove(index);
-        }
-    }
-}
+            
+           if (locationCells.isEmpty()) {
+               result = "Потопил";
+               System.out.println("Ой! Вы потопили" + name + "  :( ");
+           } else {
+               result = "Попал";
+           } //Конец if
+        } //Конец if
+         return result;
+         
+         
+    } //Конец метода
+} //Конец класса
--- LabWorks/src/labworks/SimpleDotCom.java
+++ LabWorks/src/labworks/SimpleDotCom.java
@@ -35,7 +35,7 @@ public class SimpleDotCom {
         System.out.println(result);
             return result;
     } // Завершаем метод
-// Завершаем класс
+}// Завершаем класс
     
     
     
