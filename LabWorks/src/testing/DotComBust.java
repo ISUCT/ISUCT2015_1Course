6dc5c71032cfe0204c2e40cb8a065fc228e474bf
unknown <stud_17@17_14.efdnew.isuct.ru>
2015-12-05 12:20:59 +0400

    testing

---
+++ LabWorks/src/testing/DotComBust.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+/**
+ *
+ * @author stud_17
+ */
+
+    import java.util.*;
+
+public class DotComBust {
+
+    private final GameHelper helper = new GameHelper();
+   private final ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame() {
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eTuys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+        System.out.println("Ваша цель - потопить три 'сайта'.");
+        System.out.println("Pets.com, eTuys.com,  Go2.com");
+        System.out.println("Попытайтесь потопить их за минимальное количество попыток");
+
+        for (DotCom dotComToSet : dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+            System.out.println(newLocation);
+            
+        }
+    }
+
+    private void startPlaying() {
+        while (!dotComsList.isEmpty()) {
+            String userGuess = helper.getUserInput("Введите число");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+   
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "Мимо";
+
+        for (DotCom dotComToTest : dotComsList) {
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Попал")) {
+                break;
+            }
+            if (result.equals("Потопил")) {
+                dotComsList.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        System.out.println("Все сайты потоплены! Ваши акции ничего не стоят :) ");
+        if (numOfGuesses <= 18) {
+            System.out.println("Это заняло у вас всего " + numOfGuesses + " попыток");
+            System.out.println("Вы успели выбраться  до того, как ваши акции утонули.");
+        } else {
+            System.out.println("Это заняло у вас много времени ( " + numOfGuesses + ") попыток.");
+            System.out.println("Рыбы водят хороводы вокруг ваших акций");
+        }
+    }
+
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
+}
