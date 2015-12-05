bde9269e1943e232fbaecaa794b32f198458bf1f
Your Name <you@e[ample.com>
2015-12-05 13:09:54 +0400

    animal

---
+++ LabWorks/src/Animal/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Animal;
+
+/**
+ *
+ * @author stud_17
+ */
+public class Animal {
+    public void eat(){}
+    public void sleep(){}
+    public void comeHome(){}
+    public void hunger(){}
+    public void roam(){}
+}
+ 
