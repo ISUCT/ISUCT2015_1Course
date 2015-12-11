20df7b9fec1889f4018f65226067f73c8f07fe9a
Silkova Anastasia <trapanastasia@gmail.com>
2015-12-11 19:44:33 +0300

    changes

--- FirstApp.java
+++ FirstApp.java
@@ -1,14 +1,14 @@
 public class FirstApp {
-<<<<<<< HEAD
+
     public static void main(String[] args){
 	System.out.println("Hello world");
 	System.out.println("You said: " + args[0]);
 	
     }
 }
-=======
+
  	public static void main (String[] args) {
 		System.out.println ("Hello world");
 	}
 } 
->>>>>>> c0660ee74655ba4c9e963a3687654a26afdadef4
+
