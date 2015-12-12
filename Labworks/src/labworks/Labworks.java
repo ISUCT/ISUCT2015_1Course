886bb87797ac24a956ebb2bd8e8fe80c0ce97497
Pryanya <leninapryanya@yandex.ru>
2015-12-12 22:09:32 +1000

    Animal.java

---
+++ Labworks/src/labworks/Labworks.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+/**
+ *
+ * @author stud_17
+ */
+public class Labworks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean logic = true; 
+        System.out.println(logic) ;
+        char charVar = 'a';
+        System.out.println(charVar);
+        byte b = 3;
+        System.out.println(b);
+        int g = 38;
+        System.out.println(g);
+        short s = 28902;
+        System.out.println(s);
+        long l = 37678;
+        System.out.println(l);
+        float f = 48;
+        System.out.println(f);
+        double d = 125;
+        System.out.println(d);
+    }   
+        
+}
