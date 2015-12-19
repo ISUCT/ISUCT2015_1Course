a42f09df77b877240fe25cc7d8784bb5a4353aca
unknown <stud_17@17_15.efdnew.isuct.ru>
2015-12-19 12:09:43 +0400

    auto

--- LabWorks/src/AutoPark/Transport.java
+++ LabWorks/src/AutoPark/Transport.java
@@ -9,5 +9,33 @@ package AutoPark;
  * @author stud_17
  */
 public class Transport {
+    Boolean isEmpty;
+    String brand;
+    String transport;
+    
+    public void sound() {
+ System.out.println("Welcome to the autopark!");
+           System.out.println(transport + " sound:");
+           System.out.println("My brand is" + brand);
+    
+       
+    }
+    public void start() {
+        System.out.println(brand + "is starting");
+        
+    }
+    public void ride() {
+        System.out.println(transport + "is riding");
+    }
+    public void stay() {
+        System.out.println(transport + "is staying");
+    }
+    public void action() {
+        if (isEmpty) {
+            stay();
+        } else {
+            ride();
+        }
+    }
     
 }
