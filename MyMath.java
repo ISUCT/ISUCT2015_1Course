146ddce53e7f8886f0572973b6501e24d00211d5
Satisfaction <you@example.com>
2016-03-02 16:39:33 +0300

    do later

---
+++ MyMath.java
@@ -0,0 +1,61 @@
+package testPackage;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestPackage {
+    
+    public testPackage() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSumm() {
+        System.out.println("summ");
+        int a = 2;
+        int b = 3;
+        int expResult = 5;
+        int result = MyMath.summ(a, b);
+        assertEquals(expResult, result); 
+    }
+    
+    @Test
+    public void testSummMinus() {
+        System.out.println("summ with minused");
+        int a = -2;
+        int b = 3;
+        int expResult = 1;
+        int result = MyMath.summ(a, b);
+        assertEquals(expResult, result); 
+    }
+    
+    @Test
+    public void testSummFloat() {
+        System.out.println("summ with minused");
+        float a = 2.5f;
+        float b = 3.1f;
+        float expResult = 5.6f;
+        float result = MyMath.summ(a, b);
+        assertEquals(expResult, result,0.001); 
+    }
+    
+}
