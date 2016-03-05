6ba5b2965478376571399e43e6b9fea65f433f71
Allevy <123ami123@mail.ru>
2016-03-05 13:03:55 +0300

    Tester complete

---
+++ LabWorks/src/Bilety/PamPam.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bilety;
+
+/**
+ *
+ * @author Akira
+ */
+public class PamPam {
+public static void main(String [] args){
+   int [] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+   int a = 1 + 2 + 3; 
+   int b = 4 + 5 + 6; 
+   int c = 7 + 8 + 9; 
+   int d = 10 + 11 + 12; 
+   int e = 13 + 14 + 15; 
+   int f = 16 + 17 + 18; 
+   int po = 18 + 19 + 20; 
+   
+   System.out.println(a); System.out.println(b); System.out.println(c); System.out.println(d); 
+
+}}
--- LabWorks/test/Testing/TesterTest.java
+++ LabWorks/test/Testing/TesterTest.java
@@ -5,8 +5,10 @@
  */
 package Testing;
 
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -17,17 +19,55 @@ public class TesterTest {
     public TesterTest() {
     }
 
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
     /**
      * Test of summ method, of class Tester.
      */
     @Test
     public void testSumm() {
-        System.out.println("summ");
+        System.out.println("summ test 1");
         int a = 2;
         int b = 3;
         int expResult = 5;
         int result = Tester.summ(a, b);
         assertEquals(expResult, result);
+        System.out.println(result);
     }
-    
+    @Test
+    public void test2Summ() {
+        System.out.println("summ test 2");
+        int a = 1;
+        int b = 12;
+        int expResult = 13;
+        int result = Tester.summ(a, b);
+        assertEquals(expResult, result);
+        System.out.println(result);
+    }
+    @Test
+    public void test3Summ() {
+        System.out.println("summ test 3");
+        int a = 5;
+        int b = 10;
+        int expResult = 15;
+        int result = Tester.summ(a, b);
+        assertEquals(expResult, result);
+        System.out.println(result);
+    }
+    @Test
+    public void test4Summ() {
+        System.out.println("summ test 4");
+        int a = -12;
+        int b = 10;
+        int expResult = -2;
+        int result = Tester.summ(a, b);
+        assertEquals(expResult, result);
+        System.out.println(result);
+    }
 }
