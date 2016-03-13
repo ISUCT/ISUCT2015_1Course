5338d7bd2ac2d19e8d3e7b860ad0625da46bbb24
Your name <you@example.com>
2016-03-13 18:54:04 +0300

    *.xml

---
+++ Labworks/src/Month/MyMath.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Month;
+
+/**
+ *
+ * @author Ирина
+ */
+public class MyMath {
+    public static int summ(int a, int b){
+    return a+b;
+    
+    }
+    public static float summ(float a, float b){
+        return a+b;
+    }
+}
+
