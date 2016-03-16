ecfcff106e9ab688505a959d5dad2dfc6f0dc86e
Ajoo <123ami123@mail.ru>
2016-03-16 19:43:56 +0400

    Add regular

---
+++ LabWorks/test/labworks/RegexnewTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labworks;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author UI
+ */
+public class RegexnewTest {
+    
+    public RegexnewTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+       
+String passport1="серия 1234 номер 123456 Иванов Иван Иванович";
+String passport2="1234123456 Иванов Иван Иванович";
+String passport3="серия 1234 номер 123456 Иванов И. И.";
+String passport4="12 34 №123456 Иванов Иван Иванович";
+
+        fail("The test case is a prototype.");
+}}
