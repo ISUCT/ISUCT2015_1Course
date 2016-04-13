0bedbb15c6d7c44f31bf2c7e6a045b848c3daa41
elya <elvirus97@mail.ru>
2016-04-13 18:33:14 +0300

    calculator

---
+++ LabWorks/test/Graph/CalculatorTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author UI
+ */
+public class CalculatorTest {
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of yCalc method, of class Calculator.
+     */
+    @Test
+    public void testYCalc() {
+        System.out.println("yCalc");
+        double x = 2.25;
+        Calculator instance = new Calculator();
+        double expResult = 2.0;
+        double result = instance.yCalc(x);
+        assertEquals(expResult, result, 0.001);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
