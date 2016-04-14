354cb0ba04bb519921a6da6eabff90cb5be56f35
unknown <stud_17@17_01.efdnew.isuct.ru>
2016-04-14 18:30:30 +0400

    WHAT IT IS

--- LabWorks/src/Banana/Plushka.java
+++ LabWorks/src/Banana/Plushka.java
@@ -12,7 +12,14 @@ public class Plushka {
         double y = (Math.pow(a, x) - Math.pow(b, x)) / (Math.log10(a/b)) * (Math.pow((a*b), (1/3)));
 return y;
     }
-
+public ArrayList<Double> Pachka(double xn, double xk, double dx, double a, double b) {
+ ArrayList<Double> y = new ArrayList<Double>(); 
+ for (double x = xn; x <= xk; x = x + dx){
+      y.add(y(x, a, b));
+ }
+ return y;
+ 
+}
 
 public ArrayList<Double> Duck(ArrayList<Double> x, double a, double b) {
 ArrayList<Double> y = new ArrayList<Double>(); 
--- LabWorks/test/Banana/PlushkaTest.java
+++ LabWorks/test/Banana/PlushkaTest.java
@@ -1,57 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Banana;
 
 import java.util.ArrayList;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author stud_17
- */
+
 public class PlushkaTest {
 
     public PlushkaTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
 
-    @AfterClass
-    public static void tearDownClass() {
-    }
 
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of main method, of class Plushka.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        Plushka.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-
-    }
-
-    /**
-     * Test of y method, of class Plushka.
-     */
     @Test
     public void testY() {
         System.out.println("y");
@@ -62,7 +24,6 @@ public class PlushkaTest {
         double expResult = 1.167;
         double result = instance.y(x, b, a);
         assertEquals(expResult, result, 0.001);
-        // TODO review the generated test code and remove the default call to fail.
 
     }
 
@@ -76,7 +37,6 @@ public class PlushkaTest {
         double expResult = 1.373;
         double result = instance.y(x, b, a);
         assertEquals(expResult, result, 0.001);
-        // TODO review the generated test code and remove the default call to fail.
 
     }
 
@@ -90,7 +50,6 @@ public class PlushkaTest {
         double expResult = 1.526;
         double result = instance.y(x, b, a);
         assertEquals(expResult, result, 0.001);
-        // TODO review the generated test code and remove the default call to fail.
 
     }
 
@@ -104,7 +63,6 @@ public class PlushkaTest {
         double expResult = 0.996;
         double result = instance.y(x, b, a);
         assertEquals(expResult, result, 0.001);
-        // TODO review the generated test code and remove the default call to fail.
 
     }
 
@@ -118,13 +76,10 @@ public class PlushkaTest {
         double expResult = 1.447;
         double result = instance.y(x, b, a);
         assertEquals(expResult, result, 0.001);
-        // TODO review the generated test code and remove the default call to fail.
 
     }
 
-    /**
-     * Test of Duck method, of class Plushka.
-     */
+    
     @Test
     public void testDuck() {
         System.out.println("Duck");
@@ -149,4 +104,28 @@ public class PlushkaTest {
         }
 
     }
-}
+
+    /**
+     * Test of Pachka method, of class Plushka.
+     */
+    @Test
+    public void testPachka() {
+        System.out.println("Pachka");
+        double xn = 3.2;
+        double xk = 6.2;
+        double dx = 0.6;
+        double a = 0.4;
+        double b = 0.8;
+        Plushka instance = new Plushka();
+        ArrayList<Double> expResult = new ArrayList<Double>();
+        expResult.add(1.449);
+        expResult.add(1.320);
+        expResult.add(1.185);
+        expResult.add(1.054);
+        expResult.add(0.932);
+        expResult.add(0.821);
+        ArrayList<Double> result = instance.Pachka(xn, xk, dx, a, b);
+           for (int i = 0; i < result.size(); i++) {
+         assertEquals(expResult.get(i), result.get(i), 0.001);
+    }
+}}
