1117791abddc95b2962f5aaadb1b5a221b6d789d
Pryanya <leninapryanya@yandex.ru>
2016-04-20 19:59:23 +1000

    *

--- Labworks/Test/Graph/CalculatorTest.java
+++ Labworks/Test/Graph/CalculatorTest.java
@@ -4,6 +4,7 @@
  */
 package Graph;
 
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,7 +19,58 @@ public class CalculatorTest {
 
     @Test
     public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
+        
          Calculator t = new Calculator();
+    }
+
+    /**
+     * Test of yCalc method, of class Calculator.
+     */
+    @Test
+    public void testYCalc() {
+        System.out.println("yCalc");
+        double x = 1.1;
+        double b = 0.48;
+        double a = 1.2;
+        Calculator instance = new Calculator();
+        double expResult = 0.0;
+        double result = instance.yCalc(x, b, a);
+        assertEquals(expResult, result, 0.001);
+       
+       
+    }
+
+    /**
+     * Test of TaskA method, of class Calculator.
+     */
+    @Test
+    public void testTaskA() {
+        System.out.println("TaskA");
+        double xn = 0.7;
+        double xk = 2.2;
+        double dx = 0.3;
+        double a = 1.2;
+        double b = 0.48;
+        Calculator instance = new Calculator();
+        ArrayList<Double> expResult = null;
+        ArrayList<Double> result = instance.TaskA(xn, xk, dx, a, b);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of TaskB method, of class Calculator.
+     */
+    @Test
+    public void testTaskB() {
+        System.out.println("TaskB");
+        ArrayList<Double> x = null;
+        double a = 1.2;
+        double b = 0.48;
+        Calculator instance = new Calculator();
+        ArrayList<Double> expResult = null;
+        ArrayList<Double> result = instance.TaskB(x, a, b);
+        assertEquals(expResult, result);
+       
     }
 }
--- Labworks/src/Graph/Calculator.java
+++ Labworks/src/Graph/Calculator.java
@@ -4,33 +4,35 @@
  * and open the template in the editor.
  */
 package Graph;
-
+import java.util.ArrayList;
 /**
  *
  * @author UI
  */
-import java.util.ArrayList;
+
 public class Calculator {
 
-     public ArrayList<Double> fn(double xn, double xk, double dx, double a, double b) {
-        ArrayList<Double> y = new ArrayList<>();
-        for (double x=xn; x<xk; x+=dx) {
-            y.add(yCalc(x, a, b));
-        }
-        return y;
-    }  
-     
-     public ArrayList<Double> zn(ArrayList<Double> x, double a, double b) {
-        ArrayList<Double> y = new ArrayList<>();
-        for(double i: x) {
-        y.add(yCalc(i, a, b));
-        }
-        return y;
-    }    
+   
 
     double yCalc(double x, double b, double a) {
         double y = (Math.pow(b, 3) + Math.pow(Math.sin(a * x), 2)) / Math.acos(x * b * x) + Math.exp(-x / 2);
         return y;
     }
 
+    public ArrayList<Double> TaskA(double xn, double xk, double dx, double a, double b ){
+       ArrayList<Double> y = new ArrayList<Double>();
+       for (double x=xn; x <= xk; x= x + dx) {
+            y.add(yCalc(x, a, b ));
+       }
+       return y;
+   }
+
+    public ArrayList<Double> TaskB(ArrayList<Double> x, double a, double b) {
+        ArrayList<Double> y = new ArrayList<Double>();
+        for (double i : x) {
+            y.add(yCalc(i, a, b));
+        }
+        return y;
+
+    }
 }
