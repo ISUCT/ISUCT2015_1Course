acf04f6960cc427e157c97477d63376801c74bf0
Pryanya <leninapryanya@yandex.ru>
2016-04-27 18:33:44 +0300

    *

--- Labworks/Test/Graph/CalculatorTest.java
+++ Labworks/Test/Graph/CalculatorTest.java
@@ -13,14 +13,14 @@ import static org.junit.Assert.*;
  * @author stud_5
  */
 public class CalculatorTest {
-    
+
     public CalculatorTest() {
     }
 
     @Test
     public void testSomeMethod() {
-        
-         Calculator t = new Calculator();
+
+        Calculator t = new Calculator();
     }
 
     /**
@@ -33,11 +33,10 @@ public class CalculatorTest {
         double b = 0.48;
         double a = 1.2;
         Calculator instance = new Calculator();
-        double expResult = 0.0;
+        double expResult = 1.679;
         double result = instance.yCalc(x, b, a);
         assertEquals(expResult, result, 0.001);
-       
-       
+
     }
 
     /**
@@ -52,25 +51,42 @@ public class CalculatorTest {
         double a = 1.2;
         double b = 0.48;
         Calculator instance = new Calculator();
-        ArrayList<Double> expResult = null;
+        ArrayList<Double> expResult = new ArrayList<Double>();
+        expResult.add(0.38);
+        expResult.add(0.37);
+        expResult.add(0.35);
+        expResult.add(0.33);
+        expResult.add(0.30);
         ArrayList<Double> result = instance.TaskA(xn, xk, dx, a, b);
-        assertEquals(expResult, result);
-        
+        for (int i = 0; i < 0; i++) {
+            assertEquals(expResult.get(i), result.get(i), 0.01);
+
+        }
     }
 
-    /**
-     * Test of TaskB method, of class Calculator.
-     */
     @Test
     public void testTaskB() {
         System.out.println("TaskB");
-        ArrayList<Double> x = null;
+        ArrayList<Double> x = new ArrayList<Double>();
+        x.add(0.35);
+        x.add(1.28);
+        x.add(3.51);
+        x.add(5.21);
+        x.add(4.16);
         double a = 1.2;
         double b = 0.48;
         Calculator instance = new Calculator();
-        ArrayList<Double> expResult = null;
-        ArrayList<Double> result = instance.TaskB(x, a, b);
-        assertEquals(expResult, result);
-       
+        ArrayList<Double> expResult = new ArrayList<Double>();
+        expResult.add(2.07);
+        expResult.add(1.12);
+        expResult.add(0.91);
+        expResult.add(0.7);
+        expResult.add(0.8);
+        ArrayList<Double> result = instance.TaskB(x, 1.2, 0.48);
+        for (int i = 0; i < result.size(); i++) {
+            assertEquals(expResult.get(i), result.get(i), 0.01);
+
+        }
+
     }
 }
--- Labworks/Test/Graph/DifferenceTest.java
+++ Labworks/Test/Graph/DifferenceTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Month;
+package Graph;
 
+import Month.Difference;
 import java.util.Calendar;
 import java.util.Date;
 import org.junit.Test;
