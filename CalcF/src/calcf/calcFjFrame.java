66100433f94f651d1318a3d6cee6dfdf594d90bd
Satisfaction <you@example.com>
2016-05-11 18:38:10 +0300

    calcF

--- CalcF/src/calcf/calcFjFrame.java
+++ CalcF/src/calcf/calcFjFrame.java
@@ -1,13 +1,17 @@
 package calcf;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class calcFjFrame extends javax.swing.JFrame {
-
+  ArrayList<Double> m ;
     public calcFjFrame() {
         initComponents();
     }
@@ -25,6 +29,9 @@ public class calcFjFrame extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         Result = new javax.swing.JTextArea();
         saveResult = new javax.swing.JButton();
+        OLR = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        LastRes = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -62,6 +69,17 @@ public class calcFjFrame extends javax.swing.JFrame {
             }
         });
 
+        OLR.setText("Open last result");
+        OLR.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OLRActionPerformed(evt);
+            }
+        });
+
+        LastRes.setColumns(20);
+        LastRes.setRows(5);
+        jScrollPane2.setViewportView(LastRes);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -78,37 +96,43 @@ public class calcFjFrame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(b, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(151, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(131, 131, 131)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(ok, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(23, 23, 23))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(saveResult)
-                .addGap(43, 43, 43))
+                .addGap(127, 127, 127)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(OLR, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(ok, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(saveResult, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(23, 23, 23))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(50, 50, 50)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(xn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(xk, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(dx, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(a, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(b, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(OLR)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(ok, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(35, 35, 35)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(10, 10, 10)
-                .addComponent(saveResult)
-                .addContainerGap(248, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(xn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(xk, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(dx, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(a, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(b, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(ok, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(saveResult))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(35, 35, 35)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(41, 41, 41)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(141, Short.MAX_VALUE))
         );
 
         pack();
@@ -118,10 +142,10 @@ public class calcFjFrame extends javax.swing.JFrame {
            
         CalcF f = new CalcF();
 
-        ArrayList<Double> m = f.f(Double.parseDouble(xn.getText()), Double.parseDouble(xk.getText()), Double.parseDouble(dx.getText()), Double.parseDouble(a.getText()), Double.parseDouble(b.getText()));
-
+        m = f.f(Double.parseDouble(xn.getText()), Double.parseDouble(xk.getText()), Double.parseDouble(dx.getText()), Double.parseDouble(a.getText()), Double.parseDouble(b.getText()));
+        Result.removeAll();
         for (Double i : m) {
-            Result.append(i.toString()+"\r\n");
+            Result.append(i.toString()+ "\r\n");
         }
 
     }//GEN-LAST:event_okActionPerformed
@@ -130,13 +154,30 @@ public class calcFjFrame extends javax.swing.JFrame {
         String save = Result.getText();
        
         try {
-            FileWriter writer = new FileWriter("Rerult.txt");
-            writer.write(save);
-            writer.close();
+            //FileWriter writer = new FileWriter("Rerult.txt");
+            //writer.write(save);
+            //writer.close();
+            FileOutputStream fileStream = new FileOutputStream("Result.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fileStream); 
+            os.writeObject(m);
+            os.close();
         } catch (IOException ex) {
             ex.printStackTrace();
-        }         
+        } 
+         
     }//GEN-LAST:event_saveResultActionPerformed
+
+    private void OLRActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OLRActionPerformed
+        ArrayList<Double> lr;
+        try {
+            FileInputStream fileStream = new FileInputStream("Result.ser");
+            ObjectInputStream os = new ObjectInputStream(fileStream); 
+            lr = (ArrayList<Double>)os.readObject();
+            LastRes.append(lr + "\r\n"); 
+        } catch (Exception ex) { 
+            ex.printStackTrace() ;
+        }
+    }//GEN-LAST:event_OLRActionPerformed
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -147,11 +188,14 @@ public class calcFjFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea LastRes;
+    private javax.swing.JButton OLR;
     private javax.swing.JTextArea Result;
     private javax.swing.JTextField a;
     private javax.swing.JTextField b;
     private javax.swing.JTextField dx;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton ok;
     private javax.swing.JButton saveResult;
     private javax.swing.JTextField xk;
