c432800f8a8fd5aaae8655b9ec627d6701899369
Satisfaction <you@example.com>
2016-09-02 16:44:23 +0300

    calc serv

--- Client-Server/src/client/server/Client.java
+++ Client-Server/src/client/server/Client.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
+
     
     public void go() {
         try {
--- Client-Server/src/client/server/Server.java
+++ Client-Server/src/client/server/Server.java
@@ -5,22 +5,35 @@ import java.net.*;
 
 public class Server {
     
+    String[] adviceList = {"Ешьте меньшими порциями", "Купите облегающие джинсы. Нет, они не делают вас полнее.", "Два слова: не годится", "Будьте честны хотя бы сегодня. Скажите своему начальнику все, что вы *на самом деле* о нем думаете.", "Возможно, вам стоит подобрать другую прическу."}; 
+
+
+    
     public void go() {
         try {
             ServerSocket serverSock = new ServerSocket(1818);
             while(true) {
-                Socket s = serverSock.accept();
-                PrintWriter writer = new PrintWriter(s.getOutputStream()); 
-                writer.println("Hello. ypu got it from server."); 
+                Socket sock = serverSock.accept();
+                PrintWriter writer = new PrintWriter(sock.getOutputStream()); 
+                String advice = getAdvice();
+                writer.println(advice); 
                 writer.close(); 
+                System.out.println(advice);
             }
+
+
         } catch(IOException ex) {
             ex.printStackTrace(); 
         }
+        
     }
-    
-     public static void main(String[] args) {
-        Server s = new Server(); 
-        s.go();
+           private String getAdvice() {    
+                int random = (int) (Math.random() * adviceList.length); 
+                return adviceList[random]; 
+            } 
+            
+        public static void main(String[] args) {
+        Server server = new Server(); 
+        server.go();
     }
-}
+} 
