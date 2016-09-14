2e7ae901745225c8da26fcdb6186b74b45811b61
unknown <stud_6@6_09.efdnew.isuct.ru>
2016-09-14 10:20:21 +0400

    ClientServer

---
+++ LabWorks/src/Graph/Client.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+/**
+ *
+ * @author stud_6
+ */
+public class Client {
+    public void jButton1() {
+        try {
+            Socket s = new Socket("127.0.0.1", 1455); 
+            InputStreamReader stream = new InputStreamReader(s.getInputStream()); 
+            BufferedReader reader = new BufferedReader(stream);
+            String message = reader.readLine(); 
+            System.out.println(message); 
+            reader.close();
+        } catch(IOException ex) {
+            ex.printStackTrace(); 
+        }
+    }
+    
+     public static void main(String[] args) {
+        Client c = new Client();
+        c.jButton1();
+    }
+    
+}
---
+++ LabWorks/src/Graph/Server.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author stud_6
+ */
+public class Server {
+     public void jButton1() {
+        try {
+            ServerSocket serverSock = new ServerSocket(1455);
+            while(true) {
+                Socket s = serverSock.accept();
+                PrintWriter writer = new PrintWriter(s.getOutputStream()); 
+                writer.println("Hello! How are you? Were are you from?"); 
+                writer.close(); 
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace(); 
+        }
+    }
+    
+     public static void main(String[] args) {
+        Server s = new Server(); 
+        s.jButton1();
+     }
+}
+    
+
