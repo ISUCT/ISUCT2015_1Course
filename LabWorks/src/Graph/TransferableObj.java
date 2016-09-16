5d74d41adb04ac5c0b973a45b49fcf69cec32dcb
masha <you@example.com>
2016-09-16 16:08:15 +0300

    *

---
+++ LabWorks/src/Graph/TransferableObj.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graph;
+
+import java.io.Serializable;
+
+
+ 
+public class TransferableObj implements Serializable {
+
+    private Double xn;
+    private Double xk;
+    private Double dx;
+    private Double a;
+    private Double b;
+
+    public Double getXn() {
+        return xn;
+
+    }
+public void setXn(Double xn) {
+        this.xn = xn;
+    }
+public Double getA() {
+        return a;
+    }
+public void setA(Double a) {
+        this.a = a;
+    }
+
+    public Double getB() {
+        return b;
+    }
+
+    public void setB(Double b) {
+        this.b = b;
+    }
+    public Double getXk(){
+        return xk;
+    }
+    public void setXk(Double xk) {
+        this.xk = xk;
+        
+    }
+    public Double getDx() {
+        return dx;
+    }
+    public void setDx(Double dx) {
+        this.dx = dx;
+    }
+}
