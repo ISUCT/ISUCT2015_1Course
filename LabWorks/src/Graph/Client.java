1d5748863f1fb04b46f50103b6f47d94c4535d8c
masha <you@example.com>
2016-09-16 15:46:18 +0300

    *

--- LabWorks/src/Graph/Client.java
+++ LabWorks/src/Graph/Client.java
@@ -1,37 +1,15 @@
 /*
-<<<<<<< HEAD
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Graph;
-=======
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package Graph;
 
->>>>>>> ea5c3276aa15402def7682ec0820fefb2698994b
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-<<<<<<< HEAD
 
-/**
- *
- * @author UI
- */
-public class Client {
-    public void jButton1() {
-        try{
-            Socket s = new Socket("127.0.0.1.",1450);
-            
-        }
-    }
-    
-=======
 /**
  *
  * @author stud_6
@@ -55,5 +33,4 @@ public class Client {
         c.jButton1();
     }
     
->>>>>>> ea5c3276aa15402def7682ec0820fefb2698994b
 }
