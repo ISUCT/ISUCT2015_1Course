fd1f2339002b92e4f117f8b3569627686221dfb8
unknown <stud_6@6_12.efdnew.isuct.ru>
2016-10-12 12:13:55 +0400

    d

---
+++ workspace/src/newpackage/MiniDuckSimulator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author stud_6
+ */
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
