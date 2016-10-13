7b77766c1192299cf6eb1ae0e19357fecad47390
demon4ik <demon4ik_lp@mail.ru>
2016-10-13 10:59:30 +0400

    start new

---
+++ WeatherData/src/weatherdata/WeatherData.java
@@ -0,0 +1,14 @@
+package weatherdata;
+
+public class WeatherData {
+
+    public void measurementsChanged() {
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+        
+        currentConditionsDisplay.update(temp, humidity, pressure);
+        statisticsDisplay.update(temp, humidity, pressure);
+        forecastDisplay.update(temp, humidity, pressure);
+    }
+}
