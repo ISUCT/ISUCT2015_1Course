47ae41fd209e122e8b4ab05206e07ebeaa335278
user <user@userl.ru>
2016-10-26 12:30:09 +0400

    IDK

---
+++ LabWorks/src/lab6/DisplayElement.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6;
+
+/**
+ *
+ * @author stud
+ */
+public interface DisplayElement {
+	public void display();
+}
