1e28ef6b73dd74c22a88d48c78d3d73a2d689b61
Maxim Ipatov <stud_6@6_07.efdnew.isuct.ru>
2016-10-26 13:08:27 +0400

    patterny2

---
+++ LabWorks/src/Patterny2/WeatherData.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Patterny2;
+
+import java.util.ArrayList;
+import java.util.Observable;
+/**
+ *
+ * @author stud_6
+ */
+public class WeatherData extends Observable {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    
+    public WeatherData(){}
+    
+    
+    
+    
+}
