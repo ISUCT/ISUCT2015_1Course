63fcf9fe992035151ebc5447c8921c8bbd1bc504
VlasovaElya <elvirus97@mail.ru>
2016-10-26 16:34:45 +0300

    patterny2

--- LabWorks/src/Patterny2/WeatherData.java
+++ LabWorks/src/Patterny2/WeatherData.java
@@ -18,7 +18,46 @@ public class WeatherData extends Observable {
     
     public WeatherData(){}
     
-    
-    
-    
+     public void registerObserver(java.util.Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(java.util.Observer o) {
+        int i = observers.indexOf(o);
+        if (i>=0){
+            observers.remove(i);
+        }
+    }
+
+ //   @Override
+ //   public void notifyObservers() {
+ //       for (int i=0; i<observers.size(); i++){
+ //       Observer observer = (Observer)observers.get(i);
+ //       observer.update(temperature, humidity, pressure);
+ //   }
+ // }
+  public void measurementsChanged() {
+      setChanged();
+      notifyObservers();
+  }  
+  public void setMeasurements (float temperature, float humidity, float pressure){
+      this.temperature = temperature;
+      this.humidity = humidity;
+      this.pressure = pressure;
+      measurementsChanged();
+  }  
+  
+  public float getTemperature(){
+      return temperature;
+  }
+  public float getHumidity(){
+      return humidity;
+  }
+  public float getPressure(){
+      return pressure;
+  }
+  
 }
+    
+    
+
