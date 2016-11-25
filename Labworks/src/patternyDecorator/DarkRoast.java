0c1e9e47d850721337639823d6515491b559d371
Pryanya <leninapryanya@yandex.ru>
2016-11-25 16:13:25 +0300

    *

---
+++ Labworks/src/patternyDecorator/DarkRoast.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patternyDecorator;
+
+/**
+ *
+ * @author UI
+ */
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+		description = "Dark Roast Coffee";
+	}
+ 
+	public double cost() {
+		return .99;
+	}
+}
