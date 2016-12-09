1596775652d40c4800c2d8f9d9c2c0a25fb10643
Your Name <you@example.com>
2016-12-09 16:29:53 +0300

    Decorator

---
+++ LabWorks/test/pattern_decorator/InputTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pattern_decorator;
+import java.io.*;
+/**
+ *
+ * @author UI
+ */
+public class InputTest {
+    public static void main(String[] args) throws IOException {
+		int c;
+
+		try {
+			InputStream in = 
+				new LowerCaseInputStream(
+					new BufferedInputStream(
+						new FileInputStream("test.txt")));
+
+			while((c = in.read()) >= 0) {
+				System.out.print((char)c);
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
